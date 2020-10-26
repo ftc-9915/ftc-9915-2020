dfa3b40e66b6351c27668155cea47b3e193d522c
s-meada <atmead03@gmail.com>
2020-10-25 17:21:15 -0700

    Change so that motor power increment values are changed by 0.01 instead of 0.001

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LauncherTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LauncherTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Tests;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Launcher Test", group="test")
@@ -23,7 +24,7 @@ public class LauncherTest extends OpMode {
 
         // Initialization values
         power = 0.0;
-        increment = 0.005;
+        increment = 0.01;
         buttonReleased = true;
     }
 
@@ -64,11 +65,11 @@ public class LauncherTest extends OpMode {
         // Adjusting increment value
         // (this does not affect the motor power until the increment is applied to the motor power)
         if(gamepad1.right_bumper && buttonReleased) {
-            increment += 0.001;
+            increment += 0.01;
             buttonReleased = false;
         }
         if(gamepad1.left_bumper && buttonReleased) {
-            increment = Math.max(increment - 0.001, 0.0);
+            increment = Math.max(increment - 0.01, 0.0);
             buttonReleased = false;
         }
 
