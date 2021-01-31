7504626061f32c3151b970facebb0dc84505595e
Nightmerp <averyz@gmail.com>
2021-01-30 19:19:30 -0800

    Update CollectorTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/CollectorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/CollectorTest.java
@@ -7,22 +7,25 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="Collector Test", group="test")
 public class CollectorTest extends OpMode {
-/*
+
     DcMotor leftFront;
     DcMotor leftBack;
     DcMotor rightFront;
     DcMotor rightBack;
 
- */
+
 
     DcMotor launcherMotor;
 
     DcMotor collectorMotor;
 
+    DcMotor wobbleMotor;
+
     Servo liftServo;
     Servo pushServo;
+    Servo wobbleServo;
 
-    Servo feederServo;
+    //Servo feederServo;
 
     // TODO: test these and edit with accurate values
     static final double SERVO_RESET_POS = 1.0;
@@ -31,6 +34,7 @@ public class CollectorTest extends OpMode {
     double launcherPower;
     double collectorPower;
     boolean collectorPush;
+    int wobbleArmPos;
 
     // Ensures that the adjustments are made each time the gamepad buttons are pressed rather than each time through loop
     boolean buttonReleased1;
@@ -41,6 +45,14 @@ public class CollectorTest extends OpMode {
     static final double NOT_PUSH_POS = 0.68;
     static final double PUSH_POS = 0.52;
 
+    // TODO: test these and edit with accurate values
+    static final int WOBBLE_ARM_SPEED = 10;
+    static final int WOBBLE_ARM_UPPER_LIMIT = 1000;
+    static final int WOBBLE_ARM_LOWER_LIMIT = 0;
+    static final double WOBBLE_CLAW_OPEN = 1.0;
+    static final double WOBBLE_CLAW_CLOSE = 0.0;
+
+
     double speed = 0.0;
     double strafe = 0.0;
     double rotation = 0.0;
@@ -48,14 +60,14 @@ public class CollectorTest extends OpMode {
     @Override
     public void init() {
         // Chassis Motors
-        /*
+
         leftFront = hardwareMap.dcMotor.get("leftFront");
         leftBack = hardwareMap.dcMotor.get("leftBack");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         rightBack = hardwareMap.dcMotor.get("rightBack");
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         leftBack.setDirection(DcMotor.Direction.REVERSE);
-        */
+
 
         // Attachment Motors
         launcherMotor = hardwareMap.dcMotor.get("launcherMotor");
@@ -66,13 +78,22 @@ public class CollectorTest extends OpMode {
         liftServo = hardwareMap.servo.get("liftServo");
         pushServo = hardwareMap.servo.get("pushServo");
 
-        feederServo = hardwareMap.servo.get("feederServo");
-        feederServo.setPosition(SERVO_RESET_POS);
+        /*
+        wobbleMotor = hardwareMap.dcMotor.get("wobbleMotor");
+        wobbleMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wobbleMotor.setTargetPosition(0);
+        wobbleMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        wobbleServo = hardwareMap.servo.get("wobbleServo");
+         */
+
+        //feederServo = hardwareMap.servo.get("feederServo");
+        //feederServo.setPosition(SERVO_RESET_POS);
 
         // Initialization values
         launcherPower = 0.0;
         collectorPower = 1.0;
         collectorPush = false;
+        wobbleArmPos = 0;
         buttonReleased1 = true;
         buttonReleased2 = true;
 
@@ -101,13 +122,13 @@ public class CollectorTest extends OpMode {
         strafe = gamepad1.right_stick_x;
         rotation = gamepad1.left_stick_x;
 
-        /*
+
         leftFront.setPower(speed + strafe + rotation);
         leftBack.setPower(speed - strafe + rotation);
         rightBack.setPower(speed + strafe - rotation);
         rightFront.setPower(speed - strafe - rotation);
 
-         */
+
 
         // Turns collector on/off
         if (gamepad1.a && buttonReleased1) {
@@ -119,6 +140,8 @@ public class CollectorTest extends OpMode {
             collectorPower = 0.0;
             buttonReleased1 = false;
         }
+
+        // Reverses collector
 
         if (gamepad1.x && buttonReleased1) {
             collectorPower *= -1;
@@ -158,6 +181,31 @@ public class CollectorTest extends OpMode {
             buttonReleased2 = false;
         }
 
+        // Lifts/Lowers Wobble Goal Arm
+        /*
+        wobbleArmPos += WOBBLE_ARM_SPEED * gamepad2.left_stick_y;
+        if (wobbleArmPos > WOBBLE_ARM_UPPER_LIMIT) {
+            wobbleArmPos = WOBBLE_ARM_UPPER_LIMIT;
+        }
+        if (wobbleArmPos < WOBBLE_ARM_LOWER_LIMIT) {
+            wobbleArmPos = WOBBLE_ARM_LOWER_LIMIT;
+        }
+        wobbleMotor.setTargetPosition(wobbleArmPos);
+        wobbleMotor.setPower(1.0);
+         */
+
+        // Opens/Closes Wobble Goal Claw
+        /*
+        if (gamepad2.a && buttonReleased2) {
+            wobbleServo.setPosition(WOBBLE_CLAW_OPEN);
+            buttonReleased2 = false;
+        }
+        if (gamepad2.b && buttonReleased2) {
+            wobbleServo.setPosition(WOBBLE_CLAW_CLOSED);
+            buttonReleased2 = false;
+        }
+         */
+
 
         // Do not adjust values again until after buttons are released (and pressed again) so the
         // adjustments are made each time the gamepad buttons are pressed rather than each time through loop
@@ -165,7 +213,7 @@ public class CollectorTest extends OpMode {
             buttonReleased1 = true;
         }
 
-        if(!gamepad2.left_bumper && !gamepad2.right_bumper && !gamepad2.x && !gamepad2.y) {
+        if(!gamepad2.left_bumper && !gamepad2.right_bumper && !gamepad2.a && !gamepad2.b && !gamepad2.x && !gamepad2.y) {
             buttonReleased2 = true;
         }
 
